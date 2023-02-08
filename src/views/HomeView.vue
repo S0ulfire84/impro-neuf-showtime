c14491892588262a7cb351f4383ef7c94aa1395e
Alejandro C. Saksida <acsaksida@gmail.com>
2023-02-08 20:30:49 +0100

    Add facebook login and see if this will stay a part of the application flow

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <facebook-login
+    :appId="appId"
+    @loggedIn="handleLogin"
+    @loginFailed="handleLoginFailed"
+  >
+</facebook-login>
   </main>
 </template>
+
+<script lang="ts" setup>
+import FacebookLogin from "../components/FacebookLogin.vue";
+const appId = "900476307757109";
+function handleLogin(response: any) {
+  console.log(response);
+}
+function handleLoginFailed(response: any) {
+  console.log(response);
+}
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: { https: true },
+  plugins: [mkcert(), vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
