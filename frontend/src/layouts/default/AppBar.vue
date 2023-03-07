0446c57b8308f392c6cdcb7442507a08da90e80f
Alejandro C. Saksida <acsaksida@gmail.com>
2023-03-07 15:04:50 +0100

    Feature - Add Vuetify to the application

---
+++ frontend/src/layouts/default/AppBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title class="bar-title">
+      <div>
+        <v-img contain height="32" src="@/assets/impro-neuf-logo.png" />
+      </div>
+      <div>It's Showtime</div>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
+
+<style scoped>
+.bar-title {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
